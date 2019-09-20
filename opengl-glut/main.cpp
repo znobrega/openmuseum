954d9ff79183789efe2638e520eb8b514e3a1eb6
Carlos Nobrega <nobregacarlosjr@gmail.com>
2019-09-20 18:32:53 -0300

    code for model loading

--- opengl-glut/main.cpp
+++ opengl-glut/main.cpp
@@ -83,6 +83,115 @@ void ground(){
 	glPopMatrix();
 }
 
+void cachorro() {
+
+	glPushMatrix();
+	GLfloat color[4] = { 0.92f, 0.814f, 0.382f, 1.0f };
+	glColor4fv(color);
+
+	GLfloat dog_specular[] = { 0.1f, 0.1f, 0.1f },
+			dog_shininess = 0.1f;
+
+	glMaterialfv(GL_FRONT, GL_SPECULAR, dog_specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, dog_shininess);
+	glMaterialf(GL_FRONT, GL_EMISSION, 0);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, color);
+
+	//torso
+	glPushMatrix();
+	glScalef(2.0f * 0.3f, 2.0f * 0.3f, 4.0f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	//legs
+	glPushMatrix();
+	glTranslated(-1 * 0.3, -2.5 * 0.3, -2 * 0.3);
+	glScalef(0.5f * 0.3f, 2.0f * 0.3f, 0.5f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(0.3f, -2.5f * 0.3f, -0.6);
+	glScalef(0.5f * 0.3f, 0.6f, 0.5f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(0.3f, -2.5f * 0.3f, 2.0 * 0.3f);
+	glScalef(0.5f * 0.3f, 2.0f * 0.3f, 0.5f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(-0.3f, -2.5f * 0.3f, 0.6);
+	glScalef(0.5f * 0.3f, 2.0f * 0.3f, 0.5f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	//tail
+	glPushMatrix();
+	glTranslated(0.0f, 0.0f, -3.8f * 0.3f);
+	glScalef(0.5f * 0.3f, 0.5f * 0.3f, 1.8f * 0.3f);
+
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	//head rotation
+	glPushMatrix();
+
+	//head
+	glPushMatrix();
+	glTranslated(0.0f, 2.5f * 0.3f, 3.0f * 0.3f);
+	glScalef(1.5f * 0.3f, 1.55f * 0.3f, 1.6f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	//nose
+	glPushMatrix();
+	glTranslated(0.0f, 2.2f * 0.3f, 4.2f * 0.3f);
+	glScalef(0.8f * 0.3f, 0.5f * 0.3f, 1.5f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	//ears
+	glPushMatrix();
+	glTranslated(-0.8f * 0.3f, 3.8f * 0.3f, 2.6f * 0.3f);
+	glScalef(0.5f * 0.3f, 0.3f, 0.5f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(0.8f * 0.3f, 3.8f * 0.3f, 2.6f * 0.3f);
+	glScalef(0.5f * 0.3f, 1.0f * 0.3f, 0.5f * 0.3f);
+	glutSolidSphere(1, 30, 30);
+	glPopMatrix();
+
+	//eyes
+	GLfloat eyes_specular[] = { 0.4f, 0.4f, 0.4f },
+			eyes_shininess = 1.0f;
+	GLfloat black[] = { 0,0,0,1};
+	glColor4fv(black);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, eyes_specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, eyes_shininess);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, black);
+
+	glPushMatrix();
+	glTranslated(0.5f * 0.3f, 3.0f * 0.3f, 4.4f * 0.3f);
+	glScalef(0.25f * 0.3f, 0.25f * 0.3f, 0.25f * 0.3f);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslated(-0.5f * 0.3f, 3.0f * 0.3f, 4.4f  * 0.3f);
+	glScalef(0.25f * 0.3f, 0.25f * 0.3f, 0.25f * 0.3f);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glPopMatrix();
+
+	glPopMatrix();
+}
+
 void wall() {
 	glEnable(GL_TEXTURE_2D);
 			glBindTexture(GL_TEXTURE_2D, ground_tex);
@@ -156,6 +265,7 @@ void Desenha(void)
 
 	ground();
 	ground();
+	cachorro();
 	background();
 	//wall();
 	//glColor3f(0.0f, 0.0f, 1.0f);
---
+++ opengl-glut/model.cpp
@@ -0,0 +1,218 @@
+Model::Model(const std::string fileName)
+{
+	modelpath = fileName;
+}
+
+Model::~Model()
+{}
+
+bool Model::importModel()
+{
+
+	std::string pFile = modelpath;
+	// Check if file exists
+	std::ifstream fin(pFile.c_str());
+	if (!fin.fail())
+	{
+		fin.close();
+	}
+	else
+	{
+		return false;
+	}
+
+	scene = importer.ReadFile(pFile, aiProcessPreset_TargetRealtime_Quality);
+
+	// If the import failed, report it
+	if (!scene)
+	{
+		return false;
+	}
+
+
+	// We're done. Everything will be cleaned up by the importer destructor
+	return true;
+}
+
+// Can't send color down as a pointer to aiColor4D because AI colors are ABGR.
+void  Model::Color4f(const aiColor4D *color)
+{
+	glColor4f(color->r, color->g, color->b, color->a);
+}
+
+void  Model::set_float4(float f[4], float a, float b, float c, float d)
+{
+	f[0] = a;
+	f[1] = b;
+	f[2] = c;
+	f[3] = d;
+}
+
+void  Model::color4_to_float4(const aiColor4D *c, float f[4])
+{
+	f[0] = c->r;
+	f[1] = c->g;
+	f[2] = c->b;
+	f[3] = c->a;
+}
+
+void  Model::apply_material(const aiMaterial *mtl)
+{
+	float c[4];
+
+	GLenum fill_mode;
+	int ret1, ret2;
+	aiColor4D diffuse;
+	aiColor4D specular;
+	aiColor4D ambient;
+	aiColor4D emission;
+	float shininess, strength;
+	int two_sided;
+	int wireframe;
+	unsigned int max;	// changed: to unsigned
+
+	int texIndex = 0;
+	aiString texPath;	//contains filename of texture
+
+	if (AI_SUCCESS == mtl->GetTexture(aiTextureType_DIFFUSE, texIndex, &texPath))
+	{
+		//bind texture
+		unsigned int texId = *textureIdMap[texPath.data];
+		glBindTexture(GL_TEXTURE_2D, texId);
+	}
+
+	set_float4(c, 0.8f, 0.8f, 0.8f, 1.0f);
+	if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_DIFFUSE, &diffuse))
+		color4_to_float4(&diffuse, c);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, c);
+
+	set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
+	if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_SPECULAR, &specular))
+		color4_to_float4(&specular, c);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
+
+	set_float4(c, 0.2f, 0.2f, 0.2f, 1.0f);
+	if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_AMBIENT, &ambient))
+		color4_to_float4(&ambient, c);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, c);
+
+	set_float4(c, 0.0f, 0.0f, 0.0f, 1.0f);
+	if (AI_SUCCESS == aiGetMaterialColor(mtl, AI_MATKEY_COLOR_EMISSIVE, &emission))
+		color4_to_float4(&emission, c);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, c);
+
+	max = 1;
+	ret1 = aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS, &shininess, &max);
+	max = 1;
+	ret2 = aiGetMaterialFloatArray(mtl, AI_MATKEY_SHININESS_STRENGTH, &strength, &max);
+	if ((ret1 == AI_SUCCESS) && (ret2 == AI_SUCCESS))
+		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess * strength);
+	else {
+		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 0.0f);
+		set_float4(c, 0.0f, 0.0f, 0.0f, 0.0f);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, c);
+	}
+
+	max = 1;
+	if (AI_SUCCESS == aiGetMaterialIntegerArray(mtl, AI_MATKEY_ENABLE_WIREFRAME, &wireframe, &max))
+		fill_mode = wireframe ? GL_LINE : GL_FILL;
+	else
+		fill_mode = GL_FILL;
+	glPolygonMode(GL_FRONT_AND_BACK, fill_mode);
+
+	max = 1;
+	if ((AI_SUCCESS == aiGetMaterialIntegerArray(mtl, AI_MATKEY_TWOSIDED, &two_sided, &max)) && two_sided)
+		glEnable(GL_CULL_FACE);
+	else
+		glDisable(GL_CULL_FACE);
+}
+
+void  Model::recursive_render(const struct aiScene *sc, const struct aiNode* nd, float scale)
+{
+	unsigned int i;
+	unsigned int n = 0, t;
+	aiMatrix4x4 m = nd->mTransformation;
+
+	aiMatrix4x4 m2;
+	aiMatrix4x4::Scaling(aiVector3D(scale, scale, scale), m2);
+	m = m * m2;
+
+	// update transform
+	m.Transpose();
+	glPushMatrix();
+	glMultMatrixf((float*)&m);
+
+	// draw all meshes assigned to this node
+	for (; n < nd->mNumMeshes; ++n)
+	{
+		const struct aiMesh* mesh = scene->mMeshes[nd->mMeshes[n]];
+
+		apply_material(sc->mMaterials[mesh->mMaterialIndex]);
+
+
+		if (mesh->mNormals == NULL)
+		{
+			glDisable(GL_LIGHTING);
+		}
+		else
+		{
+			glEnable(GL_LIGHTING);
+		}
+
+		if (mesh->mColors[0] != NULL)
+		{
+			glEnable(GL_COLOR_MATERIAL);
+		}
+		else
+		{
+			glDisable(GL_COLOR_MATERIAL);
+		}
+
+		for (t = 0; t < mesh->mNumFaces; ++t) {
+			const struct aiFace* face = &mesh->mFaces[t];
+			GLenum face_mode;
+
+			switch (face->mNumIndices)
+			{
+			case 1: face_mode = GL_POINTS; break;
+			case 2: face_mode = GL_LINES; break;
+			case 3: face_mode = GL_TRIANGLES; break;
+			default: face_mode = GL_POLYGON; break;
+			}
+
+			glBegin(face_mode);
+
+			for (i = 0; i < face->mNumIndices; i++)		// go through all vertices in face
+			{
+				int vertexIndex = face->mIndices[i];	// get group index for current index
+				if (mesh->mColors[0] != NULL)
+					Color4f(&mesh->mColors[0][vertexIndex]);
+				if (mesh->mNormals != NULL)
+
+					if (mesh->HasTextureCoords(0))		//HasTextureCoords(texture_coordinates_set)
+					{
+						glTexCoord2f(mesh->mTextureCoords[0][vertexIndex].x, 1 - mesh->mTextureCoords[0][vertexIndex].y); //mTextureCoords[channel][vertex]
+					}
+
+				glNormal3fv(&mesh->mNormals[vertexIndex].x);
+				glVertex3fv(&mesh->mVertices[vertexIndex].x);
+			}
+			glEnd();
+		}
+	}
+
+	// draw all children
+	for (n = 0; n < nd->mNumChildren; ++n)
+	{
+		recursive_render(sc, nd->mChildren[n], scale);
+	}
+
+	glPopMatrix();
+}
+
+void Model::renderTheModel()				//Here's where we do all the drawing
+{
+	glEnable(GL_TEXTURE_2D);
+	recursive_render(scene, scene->mRootNode, 0.35);
+	glDisable(GL_TEXTURE_2D);
+}
